eb5c083e6715d1177f027420fc9418749d5bc1d2
sauvik <sauvik@logic-square.com>
2025-03-24 15:37:29 +0530

    Configure Next.js to ignore ESLint and TypeScript errors during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // ignore eslint errors during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // ignore typescript errors during build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
 export default nextConfig;
